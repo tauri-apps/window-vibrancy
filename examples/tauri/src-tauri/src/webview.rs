9fb40c774145bad5f52d6c5f539695f19fa531a8
ahonn <yuexunjiang@gmail.com>
2025-11-26 22:49:53 +1100

    refactor(examples): replace msg_send with objc2 type-safe interfaces

--- examples/tauri/src-tauri/src/webview.rs
+++ examples/tauri/src-tauri/src/webview.rs
@@ -1,29 +1,20 @@
-use objc2::{msg_send, runtime::AnyObject};
 use objc2_app_kit::NSView;
-use objc2_foundation::NSArray;
+use objc2_foundation::NSObjectProtocol;
 use std::ptr::NonNull;
 
 pub fn find_webview_recursive(view: &NSView) -> Option<NonNull<NSView>> {
-    unsafe {
-        if let Some(wk_class) = objc2::runtime::AnyClass::get(c"WKWebView") {
-            let is_webview: bool = msg_send![view, isKindOfClass: wk_class];
-            if is_webview {
-                return NonNull::new(view as *const _ as *mut NSView);
-            }
+    if let Some(wk_class) = objc2::runtime::AnyClass::get(c"WKWebView") {
+        if view.isKindOfClass(wk_class) {
+            return NonNull::new(view as *const _ as *mut NSView);
         }
+    }
 
-        let subviews_ptr: *mut AnyObject = msg_send![view, subviews];
-        if subviews_ptr.is_null() {
-            return None;
-        }
-
-        let subviews: &NSArray<NSView> = &*(subviews_ptr as *const NSArray<NSView>);
-        let count = subviews.count();
-        for idx in 0..count {
-            let subview = subviews.objectAtIndex(idx);
-            if let Some(found) = find_webview_recursive(subview.as_ref()) {
-                return Some(found);
-            }
+    let subviews = view.subviews();
+    let count = subviews.count();
+    for idx in 0..count {
+        let subview = subviews.objectAtIndex(idx);
+        if let Some(found) = find_webview_recursive(subview.as_ref()) {
+            return Some(found);
         }
     }
 
